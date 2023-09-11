bb703e151e9fa0bed602446c2de02e2abeb447df
dennis ko <dennis0324@naver.com>
2023-09-12 01:17:59 +0900

    removing: unsused log winstonLogger

--- packages/next-nest/src/main.ts
+++ packages/next-nest/src/main.ts
@@ -4,7 +4,6 @@ import { SocketIoAdapter } from './server/socket-io.adapter';
 import { winstonLogger } from './utils/winston.config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useLogger(winstonLogger);
   app.useWebSocketAdapter(new SocketIoAdapter(app))
   await app.listen(3000);
 }
