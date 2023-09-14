c06627b6e5cccad02c850aae91ac2d549e5b3dc9
dennis ko <dennis0324@naver.com>
2023-09-14 23:30:46 +0900

    refactor: event handler move to event folder

--- packages/discord/index.ts
+++ packages/discord/index.ts
@@ -1,15 +1,17 @@
 // Require the necessary discord.js classes
-import { ChannelType, Events, ForumChannel, GatewayIntentBits, Interaction, Partials } from 'discord.js';
+import { Events, GatewayIntentBits, Interaction, Partials, } from 'discord.js';
 import { token } from './config';
-import { onReady } from "./src/events/ready";
-import { oneHundred } from "./src/commands/testing/onHundred";
-import clientManager, { BloDClient } from 'src/utility/blod-client';
-import { Manager } from 'socket.io-client';
+import { onReady } from "@/events/ready";
+import { onInteractionCreate } from '@/events/interaction-create';
+import { BloDClient } from 'src/utility/blod-client';
 import { BLoDDiscordSocket } from '@/socket-io';
+import { onMessageUpdate } from '@/events/message-update';
+import { onMessageCreate } from '@/events/message-create';
 
 
 async function run(){
-
+  
+  // console.log(adaptor.textContent(svg.styleSheet(html)));
   const client = new BloDClient(
     {
       intents: [
@@ -31,71 +33,19 @@ async function run(){
     );
   const discordSocket = new BLoDDiscordSocket(client)
 
-  discordSocket.ready().then(() => {
-    const channelsData:Record<string,string> = {}
-    console.log('ready to send')
-
-    const guildCache = discordSocket.getClient().guilds.cache
-    if(guildCache.size > 1) console.warn('discord server is more than 1');
-    const guild = guildCache.first()
-    const channels = guild.channels.cache.filter(channel => channel.type === ChannelType.GuildForum)
-    channels.forEach((channel,channelTitle) => {
-      const serverID = channelTitle
-      const title = channel.name
-
-      channelsData[title] = serverID
-    })
-    discordSocket.getSocket().emit("discord:ready",{id:guild.id,routes:channelsData})
-  })
-
-
-  //TODO: 자동으로 디스코드 서버와 연결 시키기
-  //TODO: 자동으로 디스코드 포럼 채널과 연결 시키기 if channle >= 2 then warn
-  // 함수 이름: regitster Scocket Event
-    //push: discord -> nestjs
-    //pull: nestjs -> discord
-    
-    // type: string
-    // handler: function
-  // socket.on("connect", () => {
-  //   console.log("connected",socket.id);
-  // })
-
-
-  /////////
-  
-  client.on("ready", async () => await onReady(discordSocket));
+  client.on(Events.ClientReady, async () => await onReady(discordSocket));
 
   // We use 'c' for the event parameter to keep it separate from the already defined 'client'
-  client.on(Events.InteractionCreate, async (interaction: Interaction ) => {
-    if (!interaction.isChatInputCommand()) return;
-    
-    const command = (interaction.client as BloDClient).commands.get(interaction.commandName);
-
-    if (!command) {
-      console.error(`No command matching ${interaction.commandName} was found.`);
-      return;
-    }
-  
-    try {
-      await command.run(interaction, client);
-    } catch (error) {
-      console.error(error);
-      if (interaction.replied || interaction.deferred) {
-        await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-      } else {
-        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-      }
-    }
-  })
+  client.on(Events.InteractionCreate, async (interaction: Interaction ) => { onInteractionCreate(discordSocket,interaction)})
 
   // Log in to Discord with your client's token
-  // testing for thread update
-  client.on(Events.MessageUpdate,async (oldMessage, newMessage) => {
-    console.log('thread update')
-  })
-  client.login(token);
+  // text update
+  // TODO: if thread channel get updated, send to socket for update
+  client.on(Events.MessageUpdate,async (oldMessage, newMessage) => onMessageUpdate(discordSocket,newMessage))
 
+  client.on(Events.MessageCreate, async (message) => onMessageCreate(discordSocket,message))
+  client.login(token);
 }
 
 run()
+
