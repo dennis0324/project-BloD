b8811ddef5d564c76232eb50230dc3bfd37da132
dennis ko <dennis0324@naver.com>
2023-09-14 23:51:48 +0900

    add: next navigation for testing purpose

--- packages/next-nest/src/components/Navigation.tsx
+++ packages/next-nest/src/components/Navigation.tsx
@@ -1,13 +1,20 @@
+'use client'
 import Link from "next/link"
+import { useRouter } from "next/navigation";
 
 export const Navigation = () => {
+  const router = useRouter()
   return (
     <nav>
       <div className="nav-selection">
-        <Link className="navlink" href={'/'}>Home</Link>
+        {/* <Link className="navlink" href={'/'}>Home</Link> */}
+        <a className="navlink" onClick={() => router.push('/')}>Home</a>
       </div>
       <div className="nav-selection">
-        <Link className="navlink" href={'/blog'}>Post</Link>
+        {/* <Link className="navlink" href={'/blog'}>Post</Link>
+         */}
+        <a className="navlink" onClick={() => router.push('/blog')}>Posts</a>
+
       </div>
       <div className="nav-selection">
         <Link className="navlink" href={'/project'}>Project</Link>
