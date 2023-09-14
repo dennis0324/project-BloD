c70371078b6025b36d26649a5270947a2080a80e
dennis ko <dennis0324@naver.com>
2023-09-14 23:36:56 +0900

    remove: socket manager

--- packages/next-nest/src/app/layout.tsx
+++ packages/next-nest/src/app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import React from 'react'
 import { Navigation } from '@src/components/Navigation'
-import { BloDSocketIO } from '@src/client/socket-io'
+import SocketProvider from '@src/context/socket-context'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,8 +17,6 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-  const BloDSocket = new BloDSocketIO()
-
   return (
     <html lang="en">
       <body className={inter.className}>
